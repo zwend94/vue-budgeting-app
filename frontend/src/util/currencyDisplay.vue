5f91c4ed008786866fcf03dfe5774a5e92c31b7b
Zach Wendland <127173062+zwend94@users.noreply.github.com>
2025-02-23 11:17:50 -0600

    Create currencyDisplay.vue
    bash not working

---
+++ frontend/src/util/currencyDisplay.vue
@@ -0,0 +1,30 @@
+<template>
+  <span>{{ formattedValue }}</span>
+</template>
+
+<script>
+export default {
+  name: 'CurrencyDisplay',
+  props: {
+    value: {
+      type: [Number, String],
+      required: true,
+    },
+    showDollarSign: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  computed: {
+    formattedValue() {
+      const numValue = Number(this.value);
+      if (isNaN(numValue)) return this.value;
+      else {
+        const formatted = numValue.toLocaleString('en-US');
+
+        return this.showDollarSign ? `$${formatted}` : formatted;
+      }
+    },
+  },
+};
+</script>
