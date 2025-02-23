d4e7070d0c777d6797647124dc460d20fb33b685
Zach Wendland <127173062+zwend94@users.noreply.github.com>
2025-02-23 11:08:23 -0600

    Create App.vue
    bash isnt working

---
+++ frontend/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+    <main>
+      <div>
+        <component
+          :is="currentPage"
+          :form-data="formData"
+          @update-form="updateForm"
+          @next-page="nextPage"
+          @prev-page="prevPage"
+        />
+      </div>
+    </main>
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  import IncomeInputs from './components/bigInputs.vue';
+  import FixedExpenses from './components/fixedExpenses.vue';
+  import IncomeStatement from './components/incomeStatement.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      IncomeInputs,
+      FixedExpenses,
+      IncomeStatement,
+    },
+    data() {
+      return {
+        currentPageIndex: 0,
+        pages: ['IncomeInputs', 'FixedExpenses', 'IncomeStatement'],
+        formData: {
+          monthlySalary: '',
+          taxBracket: '',
+          otherIncome: '',
+          expOne: '',
+          expTwo: '',
+          expThree: '',
+          expFour: '',
+          expFive: '',
+          expSix: '',
+          expSeven: '',
+        },
+      };
+    },
+    computed: {
+      currentPage() {
+        return this.pages[this.currentPageIndex];
+      },
+    },
+    methods: {
+      updateForm(data) {
+        this.formData = { ...this.formData, ...data };
+      },
+      nextPage() {
+        if (this.currentPageIndex < this.pages.length - 1) {
+          this.currentPageIndex++;
+        }
+      },
+      prevPage() {
+        if (this.currentPageIndex > 0) {
+          this.currentPageIndex--;
+        }
+      },
+    },
+  };
+  </script>
+  <style></style>
+  
