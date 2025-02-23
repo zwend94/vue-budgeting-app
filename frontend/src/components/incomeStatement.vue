8fcfc8dd99b4a4676888170f10ab6a5722df304c
zwend94 <zmwendland@gmail.com>
2025-02-23 12:30:13 -0600

    Computes for total expenses and income after taxes added, Fixed expense line items added. Compute for income - fixed expenses created

--- frontend/src/components/incomeStatement.vue
+++ frontend/src/components/incomeStatement.vue
@@ -24,7 +24,28 @@ export default {
     otherInc() {
       return Number(this.formData.otherIncome);
     },
+    totalIncome() {
+      return this.salary + this.otherInc;
+    },
+    incomeAfterTax() {
+      return (this.salary + this.otherInc) * (1 - this.formData.taxBracket);
+    },
+    individualExpenses() {
+      return [
+        Number(this.formData.expOne),
+        Number(this.formData.expTwo),
+        Number(this.formData.expThree),
+        Number(this.formData.expFour),
+        Number(this.formData.expFive),
+        Number(this.formData.expSix),
+        Number(this.formData.expSeven),
+        Number(this.formData.expEight),
+      ]
+    },
+    totalExpenses() {
+    return this.individualExpenses.reduce((acc, curr) => acc + curr, 0)
   },
+ },
 };
 </script>
 
@@ -38,7 +59,6 @@ export default {
       </div>
     </header>
 
-    <!-- Pro Forma Table  -->
     <div class="main-content">
       <h3 class="side-title">Monthly P/L</h3>
       <table>
@@ -60,13 +80,65 @@ export default {
             <td><CurrencyDisplay :value="otherInc" /></td>
           </tr>
           <tr>
-            <td class="table-label-cell">Total Income</td>
+            <td class="table-label-cell">Earnings Before Taxes</td>
             <td>
-              <CurrencyDisplay class="net-income" :value="salary + otherInc" />
+              <CurrencyDisplay :value="salary + otherInc" />
             </td>
           </tr>
-          <!-- Add calculation for total Income
-    Add expenses -->
+          <tr>
+            <td class="table-label-cell">Income After Taxes</td>
+            <td>
+              <CurrencyDisplay class="net-income" :value="incomeAfterTax" />
+            </td>
+          </tr>
+          <tr>
+            <td class="table-label-cell">Housing Costs</td>
+            <td>
+              <CurrencyDisplay :value="individualExpenses[0]" />
+            </td>
+          </tr>
+          <tr>
+            <td class="table-label-cell">Internet</td>
+            <td>
+              <CurrencyDisplay :value="individualExpenses[1]" />
+            </td>
+          </tr>
+          <tr>
+            <td class="table-label-cell">Cell Phone</td>
+            <td>
+              <CurrencyDisplay :value="individualExpenses[2]" />
+            </td>
+          </tr>
+          <tr>
+            <td class="table-label-cell">Car Payment</td>
+            <td>
+              <CurrencyDisplay :value="individualExpenses[3]" />
+            </td>
+          </tr>
+          <tr>
+            <td class="table-label-cell">Car Insurance</td>
+            <td>
+              <CurrencyDisplay :value="individualExpenses[4]" />
+            </td>
+          </tr>
+          <tr>
+            <td class="table-label-cell">Other Expenses</td>
+            <td>
+              <CurrencyDisplay :value="individualExpenses[5]" />
+            </td>
+          </tr>
+          <tr>
+            <td class="table-label-cell">Total Fixed Expenses</td>
+            <td>
+              <CurrencyDisplay class="net-income" :value="totalExpenses" />
+            </td>
+          </tr>
+          <tr>
+            <td class="table-label-cell">Operating Income</td>
+            <td>
+              <CurrencyDisplay class="net-income" :value="totalIncome-totalExpenses" />
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
