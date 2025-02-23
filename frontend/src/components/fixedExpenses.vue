730488aeaa807a1e50b9c91e6af938a990861e10
Zach Wendland <127173062+zwend94@users.noreply.github.com>
2025-02-23 11:15:09 -0600

    Create fixedExpenses.vue
    bash not working

---
+++ frontend/src/components/fixedExpenses.vue
@@ -0,0 +1,102 @@
+<script>
+export default {
+  name: 'FixedExpenses',
+  props: {
+    formData: {
+      type: Object,
+      required: true,
+      default: () => ({
+        expOne: '',
+        expTwo: '',
+        expThree: '',
+        expFour: '',
+        expFive: '',
+        expSix: '',
+        expSeven: '',
+      }),
+    },
+  },
+  data() {
+    return {
+      localData: {
+        ...this.formData,
+      },
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.$emit('update-form', this.localData);
+      this.$emit('next-page');
+    },
+  },
+};
+</script>
+<template>
+  <div class="page">
+    <form method="post" @submit.prevent="handleSubmit">
+      <fieldset>
+        <legend>Fixed Expenses</legend>
+        <div class="input-row">
+          <label>Transportation</label>
+          <input
+            v-model="localData.expOne"
+            type="text"
+            placeholder="Enter amount."
+          />
+        </div>
+        <div class="input-row">
+          <label>Electric Bill</label>
+          <input
+            v-model="localData.expTwo"
+            type="text"
+            placeholder="Enter amount."
+          />
+        </div>
+        <div class="input-row">
+          <label>Internet</label>
+          <input
+            v-model="localData.expThree"
+            type="text"
+            placeholder="Enter amount."
+          />
+        </div>
+        <div class="input-row">
+          <label>Cell Phone</label>
+          <input
+            v-model="localData.expFour"
+            type="text"
+            placeholder="Enter amount."
+          />
+        </div>
+        <div class="input-row">
+          <label>Car Payment</label>
+          <input
+            v-model="localData.expFive"
+            type="text"
+            placeholder="Enter amount."
+          />
+        </div>
+        <div class="input-row">
+          <label>Car Insurance</label>
+          <input
+            v-model="localData.expSix"
+            type="text"
+            placeholder="Enter amount."
+          />
+        </div>
+        <div class="input-row">
+          <label>Other</label>
+          <input
+            v-model="localData.expSeven"
+            type="text"
+            placeholder="Enter amount."
+          />
+        </div>
+      </fieldset>
+      <div class="button-group">
+        <button type="button" @click="$emit('prev-page')">Back</button>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
